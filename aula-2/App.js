c3eedcf68a78e42e292e9b3cd538df6c30a42759
Markimg22 <marquim.net.221200@hotmail.com>
2024-09-03 21:15:30 -0400

    feat: aula2

--- aula-2/App.js
+++ aula-2/App.js
@@ -1,19 +1,51 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { Button } from './src/components/Button'
 
-function Capslock(props) {
-  // JSX (Javascript + XML) => Javascript
-  return (
-    <Text>{props.texto.toUpperCase()}</Text>
-  );
-}
+// // Componente Button
+// function Button(props) {
+//   // JSX => Javascript + XML, sintaxe do React
+//   return (
+//     <Text
+//       style={{
+//         fontSize: 30,
+//         color: props.color
+//       }}
+//     >
+//       {props.texto}
+//     </Text>
+//   );
+// }
+
+// function CapsLock(props) {
+//   return (<Text>{props.children}</Text>)
+// }
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <StatusBar style="auto" />
+//       <Button texto="Olá" color="green" />
+//       <Button texto="Mundo" color="white" />
+//       <Button texto="Bem-vindo!" color="yellow" />
+//       <CapsLock>Olá</CapsLock>
+//     </View>
+//   );
+// }
 
 export default function App() {
+  const [contador, setContador] = useState(0)
+
+  const contar = () => {
+    setContador(contador + 1)
+  }
+
   return (
     <View style={styles.container}>
-      <Capslock texto="Olá mundo!" />
-      <Capslock texto="Olá" />
-      <StatusBar style="auto" />
+      <Text style={styles.texto}>Olá</Text>
+      <Text style={styles.button}>Mundo!</Text>
+      <Button />
     </View>
   );
 }
@@ -21,8 +53,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center"
   },
+  texto: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#000"
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    fontSize: 30,
+    color: "white",
+    margin: 30
+  }
 });
